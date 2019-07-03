2e1e10dc577a45a1e4108473db3c81fa1654ad1f
何志志 <951564399@qq.com>
2019-07-03 11:18:01 +0800

    新增section是否为0的判断

--- HZPlaceHolderView_Example/HZPlaceHolderView/UITableview_Extension.swift
+++ HZPlaceHolderView_Example/HZPlaceHolderView/UITableview_Extension.swift
@@ -51,21 +51,52 @@ extension UITableView {
     
     fileprivate func hz_checkEmpty() {
         
+        // 是否要显示空数据视图
         var isEmpty = true
         
+        // 需要显示空数据视图的情况:
+        // section为默认1, row为0
+        // section为0, row默认为1
+        // section为0, row为0
+        
+//        let src: UITableViewDataSource? = self.dataSource
+//        var sections: Int = 1
+//        if let _newSection = src?.numberOfSections?(in: self) {
+//            sections = _newSection
+//        }
+//        if let _src = src, self.placeHolderDelegate == nil  {
+//            self.placeHolderDelegate = _src as? HZTableViewPlaceHolderDelegate
+//        }
+//        for i in 0 ..< sections {
+//            let rows = src?.tableView(self, numberOfRowsInSection: i)
+//            if let _rows = rows, _rows > 0 {
+//                isEmpty = false
+//                break
+//            }
+//        }
+        
         let src: UITableViewDataSource? = self.dataSource
-        var sections: Int = 1
-        if let _newSection = src?.numberOfSections?(in: self) {
-            sections = _newSection
-        }
+        
         if let _src = src, self.placeHolderDelegate == nil  {
             self.placeHolderDelegate = _src as? HZTableViewPlaceHolderDelegate
         }
-        for i in 0 ..< sections {
-            let rows = src?.tableView(self, numberOfRowsInSection: i)
-            if let _rows = rows, _rows > 0 {
-                isEmpty = false
-                break
+        
+        var sections: Int = 1   // 若不实现numberOfSections方法, 默认是有1个分组
+        var isMoreSection: Bool = false    // 是否有多个section
+        if let _newSections = src?.numberOfSections?(in: self) {   // 若代理实现了numberOfSections方法, 默认有多个分组, 需先判断section是否为0
+            isMoreSection = true
+            sections = _newSections
+        }
+
+        if isMoreSection {   // 如果有多组, 先判断组的个数是否为0
+            isEmpty = sections == 0
+        }else {
+            for i in 0 ..< sections {
+                let rows = src?.tableView(self, numberOfRowsInSection: i)
+                if let _rows = rows, _rows > 0 {
+                    isEmpty = false
+                    break
+                }
             }
         }
         
